14fc53fad0a03b08c7955e7ed03770507b9878b4
Jaewon Kim <124026092+CanDoJaewon@users.noreply.github.com>
2024-03-05 17:46:20 -0800

    Add files via upload

---
+++ Unit 2 Project: Wordle Pt. 2/Unit 2 Project: Wordle Pt. 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Unit 2 Project: Wordle Pt. 2
+//
+//  Created by 김재원 on 3/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
